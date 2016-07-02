5e9faa9461a8e5fe96d1f01a1fb82957dad04c9b
Da Zhang <daz1@student.unimelb.edu.au>
2016-07-03 01:46:25 +1000

    fix

--- wechat/msg.py
+++ wechat/msg.py
@@ -34,8 +34,8 @@ class Message:
     def imgMsg(self, msg, text):
         response = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]>" \
                    "</FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType>" \
-                   "<Content><![CDATA[%s]]></Content></xml>"
-        response = response % (msg['FromUserName'], msg['ToUserName'], str(int(time.time())), 'text', msg['MediaId'])
+                   "<Image><MediaId><![CDATA[%s]]></MediaId></Image></xml>"
+        response = response % (msg['FromUserName'], msg['ToUserName'], str(int(time.time())), 'image', msg['MediaId'])
         return response
 
 # temporary test
