0b015efe246bac4aa67b3d5a9b4d698822709c93
Da Zhang <daz1@student.unimelb.edu.au>
2016-07-02 15:13:33 +1000

    check function

---
+++ wechat/utils.py
@@ -0,0 +1,20 @@
+import hashlib
+
+# init token.
+TOKEN = "wexin"
+
+# check signature from wechat server.
+def checkSignature(request):
+    global TOKEN
+    signature = request.GET.get("signature", None)
+    timestamp = request.GET.get("timestamp", None)
+    nonce = request.GET.get("nonce", None)
+    echoStr = request.GET.get("echostr",None)
+    token = TOKEN
+    tmpList = [token,timestamp,nonce]
+    tmpstr = "%s%s%s" % tuple(tmpList.sort())
+    tmpstr = hashlib.sha1(tmpstr).hexdigest()
+    if tmpstr == signature:
+        return echoStr
+    else:
+        return None
--- wechat/views.py
+++ wechat/views.py
@@ -1,6 +1,16 @@
+# coding: utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.template import RequestContext
+import time
+from django.views.decorators.csrf import csrf_exempt
+from utils import checkSignature
 
-def index(request):
-    return HttpResponse("login page")
+# check the service
+@csrf_exempt
+def check(request):
+    if request.method == 'GET':
+        response = HttpResponse(checkSignature(request),content_type="text/plain")
+        return response
+    else:
+        return None
